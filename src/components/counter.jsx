caac234ff97fa99aa20ddd931ee39334c15e5f93
OsaeAddo <immanueloaddo@gmail.com>
2021-10-24 12:51:41 +0000

    dynamic rendering

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,6 +5,11 @@ class Counter extends React.Component {
         count: 0,
     };
 
+    renderTags() {
+        if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+        return <ul>{ this.state.tags.map(tag => <li key={tag}>{ tag }</li>) }</ul>        
+    }
+
     // Applying custom styles to elements
     styles = {
         fontSize: 20,
