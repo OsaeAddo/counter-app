6b065495e88da2c7c39f77dc1fd3c91090c3e1d9
OsaeAddo <immanueloaddo@gmail.com>
2021-10-24 12:54:29 +0000

    dynamic rendering

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -24,17 +24,8 @@ class Counter extends React.Component {
         </React.Fragment>
         );
     }
-    
-    getBadgeClasses() {
-        let classes = "badge m-2 badge-";
-        classes += (this.state.count === 0) ? "warning" : "primary";
-        return classes;
-    }
 
-    formatCount() {
-        const { count } = this.state;
-        return count === 0 ? 'Zero' : count;
-    }
+    
 }
 
 export default Counter;
