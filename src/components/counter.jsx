8026075d66c5e2ce15e0cf48f5338fc415bcf662
OsaeAddo <immanueloaddo@gmail.com>
2021-11-15 17:15:14 +0000

    remove render tags function

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,16 +5,7 @@ class Counter extends React.Component {
     count: 0,
   };
 
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
-    return (
-      <ul>
-        {this.state.tags.map((tag) => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    );
-  }
+  
 
   // Applying custom styles to elements
   styles = {
